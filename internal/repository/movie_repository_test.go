54b09fddffcdfce9a70527e4d694362161e51d36
twotwobread <dltndud9137@naver.com>
2024-08-16 23:26:01 +0900

    feat: add test method compare to CreateMovieInput and Movie

--- internal/repository/movie_repository_test.go
+++ internal/repository/movie_repository_test.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"context"
+	"reflect"
 	"testing"
 	"time"
 
@@ -30,6 +31,26 @@ func (suite *MovieRepoSuite) TearDownSuite() {
 	suite.client.Close()
 }
 
+func (suite *MovieRepoSuite) assertMovieInputFields(expected, actual *domain.Movie) {
+	// Reflection-based field comparison
+	vExpected := reflect.ValueOf(expected).Elem()
+	vActual := reflect.ValueOf(actual).Elem()
+
+	// Iterate over the struct fields and assert equality
+	for i := 0; i < vExpected.NumField(); i++ {
+		fieldName := vExpected.Type().Field(i).Name
+
+		// Skip auto-generated fields like ID, CreatedAt, UpdatedAt
+		if fieldName == "ID" || fieldName == "CreatedAt" || fieldName == "UpdatedAt" {
+			continue
+		}
+
+		expectedValue := vExpected.Field(i).Interface()
+		actualValue := vActual.Field(i).Interface()
+		suite.Equalf(expectedValue, actualValue, "Field %s should be equal", fieldName)
+	}
+}
+
 var releaseDate = domain.CustomTime{Time: time.Date(2023, 12, 15, 0, 0, 0, 0, time.UTC)}
 
 func (suite *MovieRepoSuite) TestMovieInMemRepositoryStore() {
@@ -56,7 +77,7 @@ func (suite *MovieRepoSuite) TestMovieInMemRepositoryStore() {
 			r := NewMovieRepository(suite.client)
 			mId, err := r.Store(suite.ctx, tt.m)
 			assert.NoError(suite.T(), err)
-			assert.NotEqual(suite.T(), nil, mId)
+			assert.NotZero(suite.T(), mId)
 		})
 	}
 }
@@ -89,12 +110,21 @@ func (suite *MovieRepoSuite) TestMovieInMemRepositoryGetByID() {
 			movie, err := r.GetByID(suite.ctx, mId)
 			assert.NoError(suite.T(), err)
 
-			assert.Equal(suite.T(), tt.m.Title, movie.Title)
-			assert.Equal(suite.T(), tt.m.Genre, movie.Genre)
-			assert.Equal(suite.T(), tt.m.Director, movie.Director)
-			assert.Equal(suite.T(), tt.m.Actors, movie.Actors)
-			assert.Equal(suite.T(), tt.m.Description, movie.Description)
-			assert.Equal(suite.T(), tt.m.ReleaseDate, movie.ReleaseDate)
+			assert.NotZero(suite.T(), movie.ID, "ID should be set by the database")
+			assert.False(suite.T(), movie.CreatedAt.IsZero(), "CreatedAt should be set by the database")
+			assert.False(suite.T(), movie.UpdatedAt.IsZero(), "UpdatedAt should be set by the database")
+
+			expectedMovie := &domain.Movie{
+				Title:       tt.m.Title,
+				Genre:       tt.m.Genre,
+				Director:    tt.m.Director,
+				Actors:      tt.m.Actors,
+				Description: tt.m.Description,
+				ReleaseDate: tt.m.ReleaseDate,
+			}
+
+			// Compare user-provided fields only
+			suite.assertMovieInputFields(expectedMovie, movie)
 		})
 	}
 }
