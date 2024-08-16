7e77484cfd1bc05af720778481c0c6a8b10052ce
twotwobread <dltndud9137@naver.com>
2024-08-16 12:16:55 +0900

    feat: add genre in movie

--- domain/movie.go
+++ domain/movie.go
@@ -6,9 +6,34 @@ import (
 	"time"
 )
 
+type Genre string
+
+const (
+	Action         Genre = "액션"
+	Adventure      Genre = "모험"
+	Animation      Genre = "애니메이션"
+	Comedy         Genre = "코미디"
+	Crime          Genre = "범죄"
+	Documentary    Genre = "다큐멘터리"
+	Drama          Genre = "드라마"
+	Family         Genre = "가족"
+	Fantasy        Genre = "판타지"
+	History        Genre = "역사"
+	Horror         Genre = "공포"
+	Music          Genre = "음악"
+	Mystery        Genre = "미스테리"
+	Romance        Genre = "로맨스"
+	ScienceFiction Genre = "SF"
+	TVMovie        Genre = "TV영화"
+	Thriller       Genre = "스릴러"
+	War            Genre = "전쟁"
+	Western        Genre = "서부"
+)
+
 type Movie struct {
 	ID          int64      `json:"id"`
 	Title       string     `json:"title"`
+	Genre       Genre      `json:"genre"`
 	Director    string     `json:"director"`
 	Actors      []string   `json:"actors"`
 	Description string     `json:"description"`
