492b53d0e8da3f7ecbe713f38ec9470784ff7dc5
twotwobread <dltndud9137@naver.com>
2024-08-17 16:51:37 +0900

    feat: add movie usecase

---
+++ movie/movie_usercase.go
@@ -0,0 +1,34 @@
+package movie
+
+import (
+	"context"
+	"fmt"
+
+	"recommand-chat-bot/domain"
+)
+
+type movieUsecase struct {
+	repo domain.MovieRepository
+}
+
+func NewMovieUsecase(repo domain.MovieRepository) domain.MovieUsecase {
+	return &movieUsecase{repo: repo}
+}
+
+func (u *movieUsecase) Store(ctx context.Context, m *domain.CreateMovieInput) (int64, error) {
+	got, err := u.repo.Store(ctx, m)
+	if err != nil {
+		return 0, fmt.Errorf("failed to store movie - %v, input: %v", err, m)
+	}
+
+	return got, nil
+}
+
+func (u *movieUsecase) GetByID(ctx context.Context, id int64) (domain.Movie, error) {
+	got, err := u.repo.GetByID(ctx, id)
+	if err != nil {
+		return domain.Movie{}, fmt.Errorf("failed to store movie - %v, input: %v", err, id)
+	}
+
+	return got, nil
+}
